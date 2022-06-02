583fd2d2c8543992e4e05c05b6e31d3e46b2023d
djamel <djamelm@github.com>
2022-06-02 18:26:09 +0200

    v1.1.0

--- rooter/json.js
+++ rooter/json.js
@@ -2,30 +2,31 @@ var userdata = "";
 var homedata = "";
 var data = require('../data');
 var path = require('path'),
-    fs =require('fs'),
+    fs = require('fs'),
     os = require('os');
 if (process.env.APPDATA) {
-	userdata = process.env.APPDATA + path.sep + "Deezloader\\";
-	homedata = os.homedir();
+    userdata = process.env.APPDATA + path.sep + "Deezloader\\";
+    homedata = os.homedir();
 } else if (process.platform == "darwin") {
-	homedata = os.homedir();
-	userdata = homedata + '/Library/Application Support/Deezloader/';
+    homedata = os.homedir();
+    userdata = homedata + '/Library/Application Support/Deezloader/';
 } else if (process.platform == "android") {
-	homedata = os.homedir() + "/storage/shared";
-	userdata = homedata + "/Deezloader/";
+    homedata = os.homedir() + "/storage/shared";
+    userdata = homedata + "/Deezloader/";
 } else if (process.platform == "linux") {
-	homedata = os.homedir();
-	userdata = homedata + "/Documents/";
+    homedata = os.homedir();
+    userdata = homedata + "/Documents/";
 } else {
-	homedata = os.homedir();
-	userdata = homedata + '/.config/Deezloader/';
+    homedata = os.homedir();
+    userdata = homedata + '/.config/Deezloader/';
 }
 var melange = (max, start = 0) => {
-    return Math.floor((max*Math.random())+start)
+    return Math.floor((max * Math.random()) + start)
 }
 var tirage = (n, max, array = new Array()) => {
-    var tablni  = new Array(), tabRetour  = new Array()
-    for(i=0;i<n;i++) {
+    var tablni = new Array(),
+        tabRetour = new Array()
+    for (i = 0; i < n; i++) {
         tablni = melange(max);
         if (tabRetour.includes(tablni) || array.includes(tablni)) {
             i--;
@@ -35,7 +36,7 @@ var tirage = (n, max, array = new Array()) => {
     }
     return tabRetour
 }
-var func_tirage = ()=>{
+var func_tirage = () => {
     var num = tirage(4, data.length);
     var classment = tirage(4, 4)
     return {
@@ -49,14 +50,31 @@ var func_tirage = ()=>{
         "classment": classment
     }
 }
+var inObject = function (object, val) {
+    for (var i in object) {
+        if (object.hasOwnProperty(i)) {
+            if (object[i] === val) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+var search = (msg) => {
+    return data.filter(function (el) {
+        return inObject(el.msg);
+    })
+}
 module.exports = (app, io) => {
-
     // socket.io
     io.on('connection', (socket) => {
         var clientIp = socket.conn.remoteAddress
         socket.emit('tirage', func_tirage())
         socket.on('tirage', (msg) => {
             socket.emit('tirage', func_tirage())
+        })
+        socket.on('search', (msg) => {
+            socket.emit('search', search())
         })
     })
 }
